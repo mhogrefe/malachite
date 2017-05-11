a610c4ee81ecabf01f315a927cea40f8ca72ffc4
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-10 21:49:39 -0400

    move get_bit helper to integer module

--- malachite-native/src/natural/logic/get_bit.rs
+++ malachite-native/src/natural/logic/get_bit.rs
@@ -25,32 +25,3 @@ impl Natural {
         }
     }
 }
-
-mod internal {
-    use natural::Natural::{self, Large, Small};
-
-    impl Natural {
-        /// An internal function used by `Integer::get_bit`. Pretends that `self` is actually
-        /// -`self` in two's complement and tests the bit at the given index.
-        pub fn get_bit_neg(&self, index: u64) -> bool {
-            match *self {
-                Small(x) => index >= 32 || ((!x).wrapping_add(1)) & (1 << index) != 0,
-                Large(ref xs) => {
-                    let limb_index = (index >> 5) as usize;
-                    if limb_index >= xs.len() {
-                        // We're indexing into the infinite suffix of 1s
-                        return true;
-                    }
-                    let limb = if xs.into_iter().take(limb_index).all(|&x| x == 0) {
-                        // All limbs below `limb_index` are zero, so we have a carry bit when we
-                        // take the two's complement
-                        (!xs[limb_index]).wrapping_add(1)
-                    } else {
-                        !xs[limb_index]
-                    };
-                    limb & (1 << (index & 0x1f)) != 0
-                }
-            }
-        }
-    }
-}
