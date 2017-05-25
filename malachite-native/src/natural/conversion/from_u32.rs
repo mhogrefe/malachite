b4131e287ccd10aca676468f8ac58f43d83c5196
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-24 23:51:22 -0400

    added time and memory for Natural::from(u32)

--- malachite-native/src/natural/conversion/from_u32.rs
+++ malachite-native/src/natural/conversion/from_u32.rs
@@ -2,6 +2,9 @@ use natural::Natural::{self, Small};
 
 /// Converts a `u32` to a `Natural`.
 ///
+/// Time: worst case O(1)
+/// Additional memory: worst case O(1)
+///
 /// # Example
 /// ```
 /// use malachite_native::natural::Natural;
