615ccb7d8f3b8c01f42a2d54cf3985fbb16ce8ac
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-30 23:30:52 -0400

    use new benchmark interface

--- malachite-test/src/natural/arithmetic/add.rs
+++ malachite-test/src/natural/arithmetic/add.rs
@@ -1,5 +1,5 @@
 use common::{gmp_natural_to_native, gmp_natural_to_num_biguint};
-use rust_wheels::benchmarks::benchmark_3;
+use rust_wheels::benchmarks::{BenchmarkOptions3, benchmark_3};
 use rust_wheels::iterators::common::EXAMPLE_SEED;
 use rust_wheels::iterators::naturals::{exhaustive_naturals, random_naturals};
 use rust_wheels::iterators::tuples::{exhaustive_pairs_from_single, random_pairs_from_single};
@@ -22,41 +22,53 @@ pub fn demo_random_natural_add(limit: usize) {
 }
 
 pub fn benchmark_exhaustive_natural_add(limit: usize, file_name: &str) {
-    benchmark_3(exhaustive_pairs_from_single(exhaustive_naturals()),
-                &(|(x, y)| x + y),
-                &(|(x, y)| x + y),
-                &(|(x, y)| x + y),
-                &(|&(ref x, ref y)| (gmp_natural_to_native(x), gmp_natural_to_native(y))),
-                &(|&(ref x, ref y)| {
-                      (gmp_natural_to_num_biguint(x), gmp_natural_to_num_biguint(y))
-                  }),
-                &(|&(ref x, ref y)| max(x.significant_bits(), y.significant_bits()) as usize),
-                limit,
-                "malachite-gmp",
-                "malachite-native",
-                "num",
-                "Natural + Natural",
-                "max(x.significant\\\\_bits(), y.significant\\\\_bits())",
-                "time (ns)",
-                &format!("benchmarks/{}", file_name));
+    benchmark_3(BenchmarkOptions3 {
+                    xs: exhaustive_pairs_from_single(exhaustive_naturals()),
+                    function_f: &(|(x, y)| x + y),
+                    function_g: &(|(x, y)| x + y),
+                    function_h: &(|(x, y)| x + y),
+                    x_to_y: &(|&(ref x, ref y)| {
+                                  (gmp_natural_to_native(x), gmp_natural_to_native(y))
+                              }),
+                    x_to_z: &(|&(ref x, ref y)| {
+                                  (gmp_natural_to_num_biguint(x), gmp_natural_to_num_biguint(y))
+                              }),
+                    x_size: &(|&(ref x, ref y)| {
+                                  max(x.significant_bits(), y.significant_bits()) as usize
+                              }),
+                    limit: limit,
+                    f_name: "malachite-gmp",
+                    g_name: "malachite-native",
+                    h_name: "num",
+                    title: "Natural + Natural",
+                    x_axis_label: "max(x.significant\\\\_bits(), y.significant\\\\_bits())",
+                    y_axis_label: "time (ns)",
+                    file_name: &format!("benchmarks/{}", file_name),
+                });
 }
 
 pub fn benchmark_random_natural_add(limit: usize, scale: u32, file_name: &str) {
-    benchmark_3(random_pairs_from_single(random_naturals(&EXAMPLE_SEED, scale)),
-                &(|(x, y)| x + y),
-                &(|(x, y)| x + y),
-                &(|(x, y)| x + y),
-                &(|&(ref x, ref y)| (gmp_natural_to_native(x), gmp_natural_to_native(y))),
-                &(|&(ref x, ref y)| {
-                      (gmp_natural_to_num_biguint(x), gmp_natural_to_num_biguint(y))
-                  }),
-                &(|&(ref x, ref y)| max(x.significant_bits(), y.significant_bits()) as usize),
-                limit,
-                "malachite-gmp",
-                "malachite-native",
-                "num",
-                "Natural + Natural",
-                "max(x.significant\\\\_bits(), y.significant\\\\_bits())",
-                "time (ns)",
-                &format!("benchmarks/{}", file_name));
+    benchmark_3(BenchmarkOptions3 {
+                    xs: random_pairs_from_single(random_naturals(&EXAMPLE_SEED, scale)),
+                    function_f: &(|(x, y)| x + y),
+                    function_g: &(|(x, y)| x + y),
+                    function_h: &(|(x, y)| x + y),
+                    x_to_y: &(|&(ref x, ref y)| {
+                                  (gmp_natural_to_native(x), gmp_natural_to_native(y))
+                              }),
+                    x_to_z: &(|&(ref x, ref y)| {
+                                  (gmp_natural_to_num_biguint(x), gmp_natural_to_num_biguint(y))
+                              }),
+                    x_size: &(|&(ref x, ref y)| {
+                                  max(x.significant_bits(), y.significant_bits()) as usize
+                              }),
+                    limit: limit,
+                    f_name: "malachite-gmp",
+                    g_name: "malachite-native",
+                    h_name: "num",
+                    title: "Natural + Natural",
+                    x_axis_label: "max(x.significant\\\\_bits(), y.significant\\\\_bits())",
+                    y_axis_label: "time (ns)",
+                    file_name: &format!("benchmarks/{}", file_name),
+                });
 }
--- malachite-test/src/natural/conversion/from_u64.rs
+++ malachite-test/src/natural/conversion/from_u64.rs
@@ -1,7 +1,7 @@
 use malachite_gmp::natural as gmp;
 use malachite_native::natural as native;
 use num;
-use rust_wheels::benchmarks::benchmark_3;
+use rust_wheels::benchmarks::{BenchmarkOptions3, benchmark_3};
 use rust_wheels::iterators::common::EXAMPLE_SEED;
 use rust_wheels::iterators::general::random_x;
 use rust_wheels::iterators::primitive_ints::exhaustive_u;
@@ -19,37 +19,41 @@ pub fn demo_random_natural_from_u64(limit: usize) {
 }
 
 pub fn benchmark_exhaustive_natural_from_u64(limit: usize, file_name: &str) {
-    benchmark_3(exhaustive_u::<u64>(),
-                &(|u| gmp::Natural::from(u)),
-                &(|u| native::Natural::from(u)),
-                &(|u| num::BigUint::from(u)),
-                &(|&u| u),
-                &(|&u| u),
-                &(|&u| (64 - u.leading_zeros()) as usize),
-                limit,
-                "malachite-gmp",
-                "malachite-native",
-                "num",
-                "Natural::from(u64)",
-                "u.significant\\\\_bits()",
-                "time (ns)",
-                &format!("benchmarks/{}", file_name));
+    benchmark_3(BenchmarkOptions3 {
+                    xs: exhaustive_u::<u64>(),
+                    function_f: &(|u| gmp::Natural::from(u)),
+                    function_g: &(|u| native::Natural::from(u)),
+                    function_h: &(|u| num::BigUint::from(u)),
+                    x_to_y: &(|&u| u),
+                    x_to_z: &(|&u| u),
+                    x_size: &(|&u| (64 - u.leading_zeros()) as usize),
+                    limit: limit,
+                    f_name: "malachite-gmp",
+                    g_name: "malachite-native",
+                    h_name: "num",
+                    title: "Natural::from(u64)",
+                    x_axis_label: "u.significant\\\\_bits()",
+                    y_axis_label: "time (ns)",
+                    file_name: &format!("benchmarks/{}", file_name),
+                });
 }
 
 pub fn benchmark_random_natural_from_u64(limit: usize, file_name: &str) {
-    benchmark_3(random_x::<u64>(&EXAMPLE_SEED),
-                &(|u| gmp::Natural::from(u)),
-                &(|u| native::Natural::from(u)),
-                &(|u| num::BigUint::from(u)),
-                &(|&u| u),
-                &(|&u| u),
-                &(|&u| (64 - u.leading_zeros()) as usize),
-                limit,
-                "malachite-gmp",
-                "malachite-native",
-                "num",
-                "Natural::from(u64)",
-                "u.significant\\\\_bits()",
-                "time (ns)",
-                &format!("benchmarks/{}", file_name));
+    benchmark_3(BenchmarkOptions3 {
+                    xs: random_x::<u64>(&EXAMPLE_SEED),
+                    function_f: &(|u| gmp::Natural::from(u)),
+                    function_g: &(|u| native::Natural::from(u)),
+                    function_h: &(|u| num::BigUint::from(u)),
+                    x_to_y: &(|&u| u),
+                    x_to_z: &(|&u| u),
+                    x_size: &(|&u| (64 - u.leading_zeros()) as usize),
+                    limit: limit,
+                    f_name: "malachite-gmp",
+                    g_name: "malachite-native",
+                    h_name: "num",
+                    title: "Natural::from(u64)",
+                    x_axis_label: "u.significant\\\\_bits()",
+                    y_axis_label: "time (ns)",
+                    file_name: &format!("benchmarks/{}", file_name),
+                });
 }
