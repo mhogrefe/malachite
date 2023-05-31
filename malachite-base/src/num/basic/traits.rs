1e13d521729e0ca660fc3bdf62c736470c86a76f
b4D8 <56940406+b4D8@users.noreply.github.com>
2023-05-31 10:56:08 +0200

    Update traits.rs
    Implementation for `NonZero*` types:
    * `One` and `Two` for both signed and unsigned variants
    * `NegativeOne` for the signed variant

--- malachite-base/src/num/basic/traits.rs
+++ malachite-base/src/num/basic/traits.rs
@@ -65,3 +65,47 @@ impl<T: One + Sized + Zero> Iverson for T {
         }
     }
 }
+
+// Implementation for `NonZero*` types:
+// * `One` and `Two` for both signed and unsigned variants
+// * `NegativeOne` for the signed variant
+macro_rules! impl_non_zero {
+    ($($t:ident),+) => {
+        $(
+            impl One for $t {
+                const ONE: Self = match Self::new(1) {
+                    Some(v) => v,
+                    None => unreachable!("1 is a valid non zero value")
+                };
+            }
+
+            impl Two for $t {
+                const TWO: Self = match Self::new(2) {
+                    Some(v) => v,
+                    None => unreachable!("2 is a valid non zero value")
+                };
+            }
+        )+
+    };
+    ($($u:ident && $i:ident),+) => {
+        $(
+            impl_non_zero!($u, $i);
+
+            impl NegativeOne for $i {
+                const NEGATIVE_ONE: Self = match Self::new(-1) {
+                    Some(v) => v,
+                    None => unreachable!("-1 is a valid non zero value")
+                };
+            }
+        )+
+    }
+}
+
+impl_non_zero!(
+    NonZeroUsize && NonZeroIsize,
+    NonZeroU128 && NonZeroI128,
+    NonZeroU64 && NonZeroI64,
+    NonZeroU32 && NonZeroI32,
+    NonZeroU16 && NonZeroI16,
+    NonZeroU8 && NonZeroI8
+);
