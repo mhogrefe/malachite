ae47e248ab0b36607be665804262ca06bf592198
florian1345 <florian1345@gmx.de>
2024-06-03 19:35:48 +0200

    refactor add optimization

--- malachite-nz/src/natural/arithmetic/add.rs
+++ malachite-nz/src/natural/arithmetic/add.rs
@@ -147,6 +147,15 @@ fn add_and_carry(x: Limb, y: Limb, carry: &mut bool) -> Limb {
     sum
 }
 
+#[inline]
+fn add_with_carry_limb(x: Limb, y: Limb, carry: Limb) -> (Limb, Limb) {
+    let result_no_carry = x.wrapping_add(y);
+    let result = result_no_carry.wrapping_add(carry);
+    let carry = Limb::from((result_no_carry < x) || (result < result_no_carry));
+
+    (result, carry)
+}
+
 // Interpreting two slices of `Limb`s as the limbs (in ascending order) of two `Natural`s, where the
 // first slice is at least as long as the second, returns a `Vec` of the limbs of the sum of the
 // `Natural`s.
@@ -230,10 +239,7 @@ pub_crate_test! {limbs_add_same_length_to_out(out: &mut [Limb], xs: &[Limb], ys:
     let mut carry = 0;
 
     for (out, (&x, &y)) in out.iter_mut().zip(xs.iter().zip(ys.iter())) {
-        let result_no_carry = x.wrapping_add(y);
-        let result = result_no_carry.wrapping_add(carry);
-        carry = Limb::from((result_no_carry < x) || (result < result_no_carry));
-        *out = result
+        (*out, carry) = add_with_carry_limb(x, y, carry);
     }
 
     carry > 0
@@ -361,10 +367,7 @@ pub_crate_test! {limbs_slice_add_same_length_in_place_left(xs: &mut [Limb], ys: 
     let mut carry = 0;
 
     for (x, &y) in xs.iter_mut().zip(ys.iter()) {
-        let result_no_carry = (*x).wrapping_add(y);
-        let result = result_no_carry.wrapping_add(carry);
-        carry = Limb::from((result_no_carry < *x) || (result < result_no_carry));
-        *x = result
+        (*x, carry) = add_with_carry_limb(*x, y, carry);
     }
 
     carry > 0
