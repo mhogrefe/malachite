3c71c6c8a4eb3efb57caa9e609c8d35ec007cb24
Mikhail Hogrefe <mhogrefe@users.noreply.github.com>
2025-02-02 22:51:56 -0500

    reverting supercrate import changes

--- malachite/src/lib.rs
+++ malachite/src/lib.rs
@@ -7,13 +7,7 @@
 // 3 of the License, or (at your option) any later version. See <https://www.gnu.org/licenses/>.
 
 #![cfg_attr(not(feature = "random"), no_std)]
-
-/// This module contains various functions that support the other crates. This includes many
-/// numeric traits and their implementation for primitive numeric types, as well as many functions
-/// for exhaustively and randomly generating values of many types.
-pub mod base {
-    pub use malachite_base::*;
-}
+pub use malachite_base::*;
 
 #[cfg(feature = "naturals_and_integers")]
 #[cfg(feature = "rationals")]
@@ -27,18 +21,4 @@ pub use malachite_nz::natural::Natural;
 pub use malachite_nz::*;
 #[cfg(feature = "naturals_and_integers")]
 #[cfg(feature = "rationals")]
-/// [`Rational`](rational::Rational), a type representing rational numbers with arbitrarily large
-/// numerators and denominators.
-pub mod rational {
-    pub use malachite_q::*;
-}
-
-/// Various types and constants dependent on whether Malachite is built using 32-bit limbs or
-/// 64-bit limbs. `Limb` is the type such that `Vec`s of limbs are used to represent the bits of a
-/// [`Natural`].
-pub mod platform {
-    #[cfg(feature = "32_bit_limbs")]
-    pub use malachite_nz::platform_32::*;
-    #[cfg(not(feature = "32_bit_limbs"))]
-    pub use malachite_nz::platform_64::*;
-}
+pub use malachite_q::Rational;
