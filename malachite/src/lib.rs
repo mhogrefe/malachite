85a716fe5646b5ad123e76335737695d5489f3a4
Mikhail Hogrefe <mhogrefe@users.noreply.github.com>
2026-06-18 19:58:47 -0400

    more docs

--- malachite/src/lib.rs
+++ malachite/src/lib.rs
@@ -6,6 +6,50 @@
 // Lesser General Public License (LGPL) as published by the Free Software Foundation; either version
 // 3 of the License, or (at your option) any later version. See <https://www.gnu.org/licenses/>.
 
+//! [Malachite](https://www.malachite.rs/) is an arbitrary-precision arithmetic library for Rust,
+//! with efficient algorithms partially derived from [GMP](https://gmplib.org/),
+//! [FLINT](https://www.flintlib.org/), and [MPFR](https://www.mpfr.org/).
+//!
+//! This is the main Malachite crate, and the recommended way to use the library. It re-exports the
+//! contents of the more specialized crates that make up Malachite, so that depending on `malachite`
+//! alone is enough:
+//!
+//! - [`Natural`] and [`Integer`], arbitrary-precision unsigned and signed integers, are re-exported
+//!   from [`malachite-nz`](https://docs.rs/malachite-nz).
+//! - [`Rational`], arbitrary-precision rational numbers, is re-exported from
+//!   [`malachite-q`](https://docs.rs/malachite-q). (Requires the `rationals` feature, which is
+//!   enabled by default.)
+//! - `Float`, arbitrary-precision floating-point numbers, is re-exported from
+//!   [`malachite-float`](https://docs.rs/malachite-float). (Requires the `floats` feature; floats
+//!   are currently experimental.)
+//! - The [`base`] module re-exports [`malachite-base`](https://docs.rs/malachite-base), which
+//!   provides the numeric traits (and their implementations for primitive types) used throughout
+//!   Malachite, along with tools for generating values for tests and benchmarks.
+//!
+//! Most conversion and arithmetic operations are expressed as traits that live in [`base`], so a
+//! common pattern is to import them from there, for example
+//! `use malachite::base::num::arithmetic::traits::Pow;`.
+//!
+//! # Examples
+//! ```
+//! use malachite::base::num::arithmetic::traits::Pow;
+//! use malachite::{Integer, Natural, Rational};
+//!
+//! // Arbitrary-precision unsigned integers, far larger than any primitive type:
+//! assert_eq!(Natural::from(2u32).pow(128), Natural::from(1u32) << 128u64);
+//!
+//! // Signed integers:
+//! assert_eq!(-Integer::from(5) * Integer::from(5), Integer::from(-25));
+//!
+//! // Exact rational arithmetic: 1/3 + 1/6 = 1/2.
+//! assert_eq!(
+//!     Rational::from_unsigneds(1u32, 3u32) + Rational::from_unsigneds(1u32, 6u32),
+//!     Rational::from_unsigneds(1u32, 2u32)
+//! );
+//! ```
+//!
+//! For documentation of individual items, follow the re-export links below.
+
 #![cfg_attr(not(any(feature = "random", feature = "std")), no_std)]
 #![forbid(unsafe_code)]
 
